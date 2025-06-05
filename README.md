7001bf9f85cd2162ec2200452177f51a7a5b48d0
Shardul Nalegave <shardul.nalegave.2005@gmail.com>
2025-06-05 20:16:13 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+
+# C-Cask
+C implementation for [Bitmask - A Log-Structured Hash Table for Fast Key/Value Data](https://riak.com/assets/bitcask-intro.pdf)
---
+++ src/ccask_records.h
@@ -0,0 +1,37 @@
+
+#ifndef CCASK_RECORD_TYPES_H
+#define CCASK_RECORD_TYPES_H
+
+#include <stdint.h>
+
+typedef struct {
+    uint32_t crc; // 32-bit Cyclic Redundancy Check (CRC)
+    uint32_t timestamp;
+    uint32_t key_size;
+    uint32_t value_size;
+    uint8_t* key;
+    uint8_t* value;
+} data_file_record_t;
+
+typedef struct {
+    uint32_t timestamp;
+    uint32_t key_size;
+    uint32_t value_size;
+    uint64_t value_pos;
+    uint8_t* key;
+} hint_file_record_t;
+
+typedef struct {
+    uint64_t file_id;
+    uint64_t value_pos;
+    uint32_t value_size;
+    uint32_t timestamp;
+} keydir_record_t;
+
+void data_file_record_serialize(data_file_record_t* record, uint8_t* buffer);
+void data_file_record_deserialize(uint8_t* buffer, data_file_record_t* record);
+
+void hint_file_record_serialize(hint_file_record_t* record, uint8_t* buffer);
+void hint_file_record_deserialize(uint8_t* buffer, hint_file_record_t* record);
+
+#endif
